e5d6e39de322d4b80b71b41feeafe29de139f9c0
a956a <68533106+a956a@users.noreply.github.com>
2021-04-26 21:39:55 +0900

    바코드 2nd

--- app/src/main/java/com/example/vegeproject/barcode/BarcodeActivity.java
+++ app/src/main/java/com/example/vegeproject/barcode/BarcodeActivity.java
@@ -2,6 +2,8 @@ package com.example.vegeproject.barcode;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,16 +18,31 @@ import com.example.vegeproject.R;
 public class BarcodeActivity extends AppCompatActivity {
 
     private IntentIntegrator Scan;
-
+    static String string;
+    Button btn1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_barcode);
+        setContentView(R.layout.activity_barcode);
+
+        btn1=(Button)findViewById(R.id.btn1);
 
         Scan = new IntentIntegrator(this);
         Scan.setOrientationLocked(false);//폰 방향대로 가로세로모드변경
         Scan.setPrompt("바코드를 카메라 중앙에 맞춰주세요.");
         Scan.initiateScan();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                result_fragment fragment1= new result_fragment();
+                transaction.replace(R.id.frame,fragment1);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+            }
+        });
 
         }
 
@@ -38,10 +55,11 @@ public class BarcodeActivity extends AppCompatActivity {
         if(result != null) {
             if(result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-                // todo
+
             } else {
                 Toast.makeText(this, "바코드값: " + result.getContents(), Toast.LENGTH_LONG).show();
-                // todo
+                string=result.getContents();
+
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
---
+++ app/src/main/java/com/example/vegeproject/barcode/result_fragment.java
@@ -0,0 +1,24 @@
+package com.example.vegeproject.barcode;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.vegeproject.R;
+
+public class result_fragment extends Fragment {
+public  result_fragment(){
+
+}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_barcode,container,false);
+    }
+}
