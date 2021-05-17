f449843359dcfa4b55540ed2ddd5f6b09420c29d
seonju0147 <48851230+seonju0147@users.noreply.github.com>
2021-05-17 18:59:18 +0900

    Firebase 연동
    - list 번호로 데이터 가져오기
    - search 화면 xml inputText 변경

--- app/src/main/java/com/example/vegeproject/search/SearchActivity.java
+++ app/src/main/java/com/example/vegeproject/search/SearchActivity.java
@@ -1,9 +1,15 @@
 package com.example.vegeproject.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +17,55 @@ import androidx.fragment.app.Fragment;
 
 import com.example.vegeproject.MainActivity;
 import com.example.vegeproject.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search);
 
+        ImageView searchButton = findViewById(R.id.searchButton) ;
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView recentSearchItem = findViewById(R.id.recentSearchItem);
+                EditText editText = findViewById(R.id.editText);
+
+                String searchItem = editText.getText().toString();
+                recentSearchItem.setText(editText.getText()); // 최근검색목록인데 이건 나중에 구현
+
+                readFirebaseList(searchItem);
+            }
+        }) ;
+
+    }
+
+    private void readFirebaseList(String searchItem){
+        databaseReference.child("list").child("9106").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                if(dataSnapshot.getValue(FirebaseList.class) != null){
+                    FirebaseList post = dataSnapshot.getValue(FirebaseList.class);
+                    Log.w("FireBaseData", "getData" + post.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
     }
 }
