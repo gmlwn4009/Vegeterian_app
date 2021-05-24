0d9c55a4a91bac6676b0ad1e357dad862269dc09
gmlwn4009 <gmlwn4009@naver.com>
2021-05-24 19:58:56 +0900

    barcode + firebase

--- app/src/main/java/com/example/vegeproject/barcode/BarcodeActivity.java
+++ app/src/main/java/com/example/vegeproject/barcode/BarcodeActivity.java
@@ -11,11 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.vegeproject.search.FirebaseList;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -25,6 +27,8 @@ import java.util.StringTokenizer;
 
 public class BarcodeActivity extends AppCompatActivity implements Barcode_getXML.BarcodeResponse, Food_info_getXML.FoodResponse {
 
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("list");
+    private FirebaseList firebaseList = new FirebaseList();
     Barcode_getXML barcode_getXML = new Barcode_getXML();
     Food_info_getXML food_info_getXML = new Food_info_getXML();
     private IntentIntegrator Scan;
@@ -44,6 +48,26 @@ public class BarcodeActivity extends AppCompatActivity implements Barcode_getXML
 
     }
 
+    private void readFirebaseList(String searchItem){
+
+        databaseReference.orderByChild("barcode").equalTo(searchItem).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data: dataSnapshot.getChildren()){
+                    firebaseList.prdlstNm = data.child("prdlstNm").getValue(String.class);
+                    firebaseList.allergy = data.child("allergy").getValue(String.class);
+                    firebaseList.barcode = data.child("barcode").getValue(String.class);
+                    Log.w("FirebaseData", firebaseList.prdlstNm + ", " + firebaseList.allergy + ", " + firebaseList.barcode);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -61,10 +85,10 @@ public class BarcodeActivity extends AppCompatActivity implements Barcode_getXML
                 Toast.makeText(this, "바코드값: " + result.getContents(), Toast.LENGTH_LONG).show();
                 string=result.getContents();
 
-                barcode_getXML.delegate = this;
-                String barcodeUrl = "https://openapi.foodsafetykorea.go.kr/api/792583e106f84aa688f1/C005/xml/0/1/BAR_CD=" +
-                        result.getContents();
-                barcode_getXML.execute(barcodeUrl);
+//                barcode_getXML.delegate = this;
+//                String barcodeUrl = "https://openapi.foodsafetykorea.go.kr/api/792583e106f84aa688f1/C005/xml/0/1/BAR_CD=" +
+//                        result.getContents();
+//                barcode_getXML.execute(barcodeUrl);
 
             }
         } else {
@@ -75,36 +99,36 @@ public class BarcodeActivity extends AppCompatActivity implements Barcode_getXML
     @Override
     public void processFinish(String output) {
         // i==0일 때 barcode XML로 부터 품목번호 받아온 뒤 food_XML url에 대입
-        if(i==0) {
+//        if(i==0) {
 //            food_info_getXML.delegate = this;
 //            String foodUrl = "http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService?ServiceKey=kVYcCisbHyjiLHSoknw1iZbhenW6Glc2mM4hfGf1EeIHjXagq6P9g98eMXs6lFGtlksA74tis6Z677Ol%2FjiHrw%3D%3D&prdlstReportNo=" +
 //                    output;
 //            food_info_getXML.execute(foodUrl);
 //            i++;
-
-            StringTokenizer arr = new StringTokenizer(output, "*");
-            String[] attr = new String[6];
-            for(int i=0;arr.hasMoreTokens();i++){
-                attr[i] = arr.nextToken();
-                Log.e("arr",attr[i]);
-            }
-
-            FirebaseDatabase database = FirebaseDatabase.getInstance("https://vegeproject-7e016-default-rtdb.asia-southeast1.firebasedatabase.app/");
-            DatabaseReference myRef = database.getReference("880123153416");
-
-            myRef.child("제품명").setValue("돈까스");
-            myRef.child("이미지").setValue("url::///");
-        }
-        // food_XML에서 받아온 정보 표시
-        else{
-            result_fragment fragment = new result_fragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.barcode_activity, fragment).commit();
-
-            Bundle bundle = new Bundle();
-            bundle.putString("output",output);
-            fragment.setArguments(bundle);
-            i--;
-        }
-        Log.e("process string",output);
+//
+//            StringTokenizer arr = new StringTokenizer(output, "*");
+//            String[] attr = new String[6];
+//            for(int i=0;arr.hasMoreTokens();i++){
+//                attr[i] = arr.nextToken();
+//                Log.e("arr",attr[i]);
+//            }
+//
+//            FirebaseDatabase database = FirebaseDatabase.getInstance("https://vegeproject-7e016-default-rtdb.asia-southeast1.firebasedatabase.app/");
+//            DatabaseReference myRef = database.getReference("880123153416");
+//
+//            myRef.child("제품명").setValue("돈까스");
+//            myRef.child("이미지").setValue("url::///");
+//        }
+//        // food_XML에서 받아온 정보 표시
+//        else{
+//            result_fragment fragment = new result_fragment();
+//            getSupportFragmentManager().beginTransaction().replace(R.id.barcode_activity, fragment).commit();
+//
+//            Bundle bundle = new Bundle();
+//            bundle.putString("output",output);
+//            fragment.setArguments(bundle);
+//            i--;
+//        }
+//        Log.e("process string",output);
     }
 }
