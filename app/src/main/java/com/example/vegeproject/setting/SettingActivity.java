712c1b97c88d6ee037e2d95fcc90da3aef57efcf
a956a <68533106+a956a@users.noreply.github.com>
2021-10-13 00:55:27 +0900

    설정탭에서 단계설정 후 SharedPreferences이용하여 상태 저장

--- app/src/main/java/com/example/vegeproject/setting/SettingActivity.java
+++ app/src/main/java/com/example/vegeproject/setting/SettingActivity.java
@@ -1,10 +1,14 @@
 package com.example.vegeproject.setting;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vegeproject.MainActivity;
@@ -13,7 +17,11 @@ import com.example.vegeproject.search.SearchActivity;
 import com.example.vegeproject.setting.SettingLevel;
 
 public class SettingActivity extends AppCompatActivity {
-    private TextView setLevel;
+
+    private TextView setLevel,personal_lev;
+    SharedPreferences pref;//프리퍼런스
+    SharedPreferences.Editor editor;//에디터
+    String pref_str;//저장될 장소
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +29,58 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
         setLevel = (TextView) findViewById(R.id.setLev);
 
+        //Shared Preference 초기화
+        pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        editor = pref.edit();
+
+        //저장해둔 값 불러오기
+        pref_str = pref.getString("Pref_str","단계를 설정해주세요:)");
+
+        //레이아웃 변수 초기화 및 앱을 켜면 이전에 저장해둔 값 표시해주기
+        personal_lev = (TextView) findViewById(R.id.level_present);
+        if(pref_str.equals("단계를 설정해주세요:)"))
+           personal_lev.setText(pref_str);
+
+        else
+            personal_lev.setText("당신은 " + pref_str + "이군요!");
+
         setLevel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(SettingActivity.this, SettingLevel.class);
-                startActivity(intent);
+                startActivityForResult(intent,1000);
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        personal_lev = (TextView) findViewById(R.id.level_present);
+
+        if (requestCode == 1000)
+        {
+
+                if (resultCode == Activity.RESULT_OK) {
+                    String sendText = data.getExtras().getString("결과");
+                    personal_lev.setText("당신은 " + sendText + "이군요!");
+                    Log.e("sendText", sendText);
+                    pref_str = data.getExtras().getString("결과");
+                    editor.putString("Pref_str", pref_str);
+                    editor.apply();
+                }
+                else if(resultCode == RESULT_CANCELED) {
+                    return;
+                }
+                else{
+
+                }
+
+
+        }
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/vegeproject/setting/SettingLevel.java
@@ -0,0 +1,104 @@
+package com.example.vegeproject.setting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vegeproject.R;
+
+public class SettingLevel extends AppCompatActivity {
+    SharedPreferences pref2;
+    SharedPreferences.Editor editor2;
+    String savedClickedData;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setlevel);
+        final RadioGroup rg = (RadioGroup) findViewById(R.id.RadioGroup1);//비건단계 라디오 그룹
+        final RadioButton r_began = (RadioButton) findViewById(R.id.lev_began);
+        final RadioButton r_lacto = (RadioButton) findViewById(R.id.lev_lacto);
+        final RadioButton r_lactovo = (RadioButton) findViewById(R.id.lev_lactovo);
+        final RadioButton r_semi = (RadioButton) findViewById(R.id.lev_semi);
+        final RadioButton r_pesco = (RadioButton) findViewById(R.id.lev_pesco);
+        final RadioButton r_flexi = (RadioButton) findViewById(R.id.lev_flexi);
+        Button b = (Button) findViewById(R.id.select_ok);//설정 완료 버튼
+
+
+        //Shared Preference 초기화
+        pref2 = getSharedPreferences("pref2", Activity.MODE_PRIVATE);
+        editor2 = pref2.edit();
+
+        //저장값 불러오기
+        savedClickedData = pref2.getString("SAVE_CHECK_DATA","");
+        //Log.e("savedClickedData",savedClickedData);
+
+        //이전에 저장된값 보여주기
+        switch (savedClickedData){
+            case "비건":
+                r_began.setChecked(true);
+                break;
+            case "락토":
+                r_lacto.setChecked(true);
+                break;
+            case "락토오보":
+                r_lactovo.setChecked(true);
+                break;
+            case "세미":
+                r_semi.setChecked(true);
+                break;
+            case "페스코":
+                r_pesco.setChecked(true);
+                break;
+            case "플렉시테리언":
+                r_flexi.setChecked(true);
+                break;
+        }
+
+        //설정 완료 버튼 클릭
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               int id = rg.getCheckedRadioButtonId();
+               RadioButton rb = (RadioButton) findViewById(id);
+
+               //아무것도 선택 안되었을때
+               if(rb == null){
+                    Toast.makeText(SettingLevel.this,"단계를 선택해주세요.",Toast.LENGTH_LONG).show();;
+                }
+               //단계가 선택되었을때
+                else {
+
+                   Intent data = new Intent();
+                   data.putExtra("결과", rb.getText().toString());
+                   savedClickedData = rb.getText().toString();
+                   editor2.putString("SAVE_CHECK_DATA",savedClickedData);
+                   editor2.apply();
+
+                   setResult(RESULT_OK, data);
+                   finish();
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+}
