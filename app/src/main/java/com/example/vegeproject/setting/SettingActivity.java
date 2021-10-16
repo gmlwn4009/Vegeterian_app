56fe4762b56ef60c41fb51bc053d2394727bc15b
a956a <68533106+a956a@users.noreply.github.com>
2021-10-16 12:58:48 +0900

    10/16 -설정탭 닉네임 설정하기

--- app/src/main/java/com/example/vegeproject/setting/SettingActivity.java
+++ app/src/main/java/com/example/vegeproject/setting/SettingActivity.java
@@ -16,31 +16,36 @@ import com.example.vegeproject.MainActivity;
 import com.example.vegeproject.R;
 import com.example.vegeproject.search.SearchActivity;
 import com.example.vegeproject.setting.SettingLevel;
+import com.example.vegeproject.setting.SettingNickname;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private TextView setLevel,personal_lev;
+    private TextView setLevel,personal_lev,setNickname,personal_nick;
     private ImageView personal_lev_img;
     SharedPreferences pref;//프리퍼런스
     SharedPreferences.Editor editor;//에디터
-    String pref_str;//저장될 장소
+    String pref_str,pref_str2;//저장될 장소
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setLevel = (TextView) findViewById(R.id.setLev);
+        setNickname = (TextView) findViewById(R.id.change_nick);
 
         //Shared Preference 초기화
         pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        pref = getSharedPreferences("prefnick",Activity.MODE_PRIVATE);
         editor = pref.edit();
 
         //저장해둔 값 불러오기
         pref_str = pref.getString("Pref_str","단계를 설정해주세요:)");
+        pref_str2 = pref.getString("Pref_nick","비거닝님, 안녕하세요.");
 
         //레이아웃 변수 초기화 및 앱을 켜면 이전에 저장해둔 값 표시해주기
         personal_lev = (TextView) findViewById(R.id.level_present);
         personal_lev_img = (ImageView) findViewById(R.id.level_img);
+        personal_nick = (TextView) findViewById(R.id.nickname);
 
         if(pref_str.equals("단계를 설정해주세요:)")) {
             personal_lev.setText(pref_str);
@@ -76,11 +81,24 @@ public class SettingActivity extends AppCompatActivity {
 
             }
         }
+        if(pref_str2.equals("비거닝님, 안녕하세요."))
+            personal_nick.setText(pref_str2);
+        else
+            personal_nick.setText(pref_str2 + "님, 안녕하세요.");
+
         setLevel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(SettingActivity.this, SettingLevel.class);
                 startActivityForResult(intent,1000);
+            }
+        });
+
+        setNickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, SettingNickname.class);
+                startActivityForResult(intent,2000);
             }
         });
 
@@ -90,7 +108,7 @@ public class SettingActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         personal_lev = (TextView) findViewById(R.id.level_present);
-
+        personal_nick = (TextView) findViewById(R.id.nickname);
         if (requestCode == 1000)
         {
 
@@ -133,6 +151,22 @@ public class SettingActivity extends AppCompatActivity {
                 else{
 
                 }
+        }
+        else if(requestCode == 2000){
+
+            if (resultCode == Activity.RESULT_OK){
+                String sendText2 = data.getExtras().getString("결과2");
+                personal_nick.setText(sendText2 +"님, 안녕하세요.");
+                pref_str2 = sendText2;
+                editor.putString("Pref_nick",pref_str2);
+                editor.apply();
+        }
+            else if(resultCode == RESULT_CANCELED) {
+                return;
+            }
+            else{
+
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/vegeproject/setting/SettingNickname.java
@@ -0,0 +1,82 @@
+package com.example.vegeproject.setting;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vegeproject.R;
+
+public class SettingNickname extends AppCompatActivity {
+   private EditText editNick;
+    SharedPreferences pref3;
+    SharedPreferences.Editor editor3;
+    String savedChangedData;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setnickname);
+        editNick = (EditText) findViewById(R.id.put_nickname);
+        Button b = (Button) findViewById(R.id.change_ok);//설정 완료 버튼
+
+
+        //Shared Preference 초기화
+        pref3 = getSharedPreferences("pref3", Activity.MODE_PRIVATE);
+        editor3 = pref3.edit();
+
+        //저장값 불러오기
+        savedChangedData = pref3.getString("SAVE_CHANGED_DATA","");
+        //Log.e("savedClickedData",savedClickedData);
+
+        //이전에 저장된값 보여주기
+        if(savedChangedData.equals(""))
+            editNick.setHint("닉네임을 입력해주세요");
+        else
+            editNick.setHint(savedChangedData);
+
+        //설정 완료 버튼 클릭
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //아무것도 선택 안되었을때
+                if(editNick == null){
+                    Toast.makeText(SettingNickname.this,"닉네임을 입력해주세요.",Toast.LENGTH_LONG).show();;
+                }
+                //단계가 선택되었을때
+                else {
+
+                    Intent data = new Intent();
+                    data.putExtra("결과2", editNick.getText().toString());
+                    savedChangedData = editNick.getText().toString();
+                    editor3.putString("SAVE_CHANGED_DATA",savedChangedData);
+                    editor3.apply();
+
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+}
