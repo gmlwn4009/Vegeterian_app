23aaf4ff2908baddfce8d790d5e56bd29956a35d
a956a <68533106+a956a@users.noreply.github.com>
2021-11-06 02:44:38 +0900

    11/6 - 최근 검색어 최종오옹

--- app/src/main/java/com/example/vegeproject/search/SearchActivity.java
+++ app/src/main/java/com/example/vegeproject/search/SearchActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.graphics.Typeface;
-import android.preference.PreferenceManager;
 import android.view.MotionEvent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -15,12 +13,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,7 +34,7 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SearchActivity extends AppCompatActivity {
@@ -52,7 +49,7 @@ public class SearchActivity extends AppCompatActivity {
     //최근 검색어 저장 array
     ArrayAdapter arrayAdapter;
     ArrayList<String> array_list;
-    Set set = new HashSet();
+    Set set = new LinkedHashSet();
     ListView listView;
 
     //Shared Preference 생성
@@ -62,10 +59,6 @@ public class SearchActivity extends AppCompatActivity {
     //getStringArrayPref() 결과값을 담아낼 ArrayList<String>
     private ArrayList<String> getStringArrayPrefList;
 
-    //Json array
-    JSONArray a = new JSONArray();
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
@@ -74,7 +67,7 @@ public class SearchActivity extends AppCompatActivity {
         ImageView searchButton = findViewById(R.id.searchButton);
         listView = findViewById(R.id.listView);//최근검색어 저장용
         TextView deleteButton = findViewById(R.id.deleteText);//전체기록 삭제
-        TextView deleteselecButton = findViewById(R.id.deleteselecText);//선택기록 삭제
+
 
         //프리퍼런스 초기화
         // Shared Preference 초기화
@@ -90,18 +83,20 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                // 검색할 키워드 입력
+                EditText editText = findViewById(R.id.editText);
+                searchItem = editText.getText().toString();
+                if (searchItem.equals("")) {
+                    Toast.makeText(SearchActivity.this, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
                 // 로딩창 객체 생성
                 progressDialog = new ProgressDialog(context);
                 progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
                 progressDialog.setCancelable(false);
                 progressDialog.show();
 
-                // 검색할 키워드 입력
-                EditText editText = findViewById(R.id.editText);
-                searchItem = editText.getText().toString();
-
-                // 최근검색목록 update
-                createTextview(searchItem);
+                    // 최근검색목록 update
+                    createTextview(searchItem);
 
                 readFirebaseData(new MyCallback() {
                     @Override
@@ -112,10 +107,11 @@ public class SearchActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
+                }
             }
         });
 
-        //최근 검색기록 클릭시 검색되는...
+    //최근 검색기록 클릭시 검색되는...
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -139,7 +135,10 @@ public class SearchActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
-
+                // 최근 검색어 클릭시 상단으로 올림
+                array_list.remove(searchItem);
+                set.remove(searchItem);
+                createTextview(searchItem);
             }
         });
 
@@ -167,31 +166,6 @@ public class SearchActivity extends AppCompatActivity {
                 }
             }
         });
-
-        //검색 기록 선택 삭제
-        deleteButton.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                int count, checked ;
-                count = arrayAdapter.getCount() ;
-
-                if (count > 0) {
-                    // 현재 선택된 아이템의 position 획득.
-                    checked = listView.getCheckedItemPosition();
-
-                    if (checked > -1 && checked < count) {
-                        // 아이템 삭제
-                        array_list.remove(checked) ;
-
-                        // listview 선택 초기화.
-                        listView.clearChoices();
-
-                        // listview 갱신.
-                        arrayAdapter.notifyDataSetChanged();
-                    }
-                }
-            }
-        }) ;
-
 
         //검색기록 전체 삭제
         deleteButton.setOnClickListener(new View.OnClickListener() {
@@ -249,23 +223,36 @@ public class SearchActivity extends AppCompatActivity {
         void onCallback(ArrayList<FirebaseData> list);
     }
 
-    //최근검색어 생성
+    //createTextview 최근검색어 생성
     private void createTextview(String searchItem) {
 
+        Collections.reverse(array_list);
+        set.clear();
+        set.addAll(array_list);
 
         set.add(searchItem);
         array_list.clear();
         array_list.addAll(set);
+        //최대 9개까지만 저장
+        if(array_list.size() >= 10) {
+
+           array_list.remove(0);
+           set.clear();
+
+            for(int i= 0; i < 9;i++)
+                set.add(array_list.get(i).toString());
+
+        }
+
+        array_list.clear();
+        array_list.addAll(set);
         Collections.reverse(array_list);
-        arrayAdapter = new ArrayAdapter(SearchActivity.this, android.R.layout.simple_list_item_multiple_choice, array_list);
-        listView.setAdapter(arrayAdapter);
 
-        //sharedpreference
-        ArrayList<String> list = new ArrayList<String>();
-        list.add(searchItem);
-        setStringArrayPref(SearchActivity.this, "RECENTSEARCH", list);
+            arrayAdapter = new ArrayAdapter(SearchActivity.this, R.layout.searchlog_card, R.id.log, array_list);
+            listView.setAdapter(arrayAdapter);
 
-    }
+            setStringArrayPref(SearchActivity.this, "RECENTSEARCH", array_list);
+        }
 
     //Shared 비우는 메소드
     public void clearShared() {
@@ -279,24 +266,24 @@ public class SearchActivity extends AppCompatActivity {
         // 저장해둔 값 불러오기
         getStringArrayPrefList = getStringArrayPref(SearchActivity.this, "RECENTSEARCH");
         if (getStringArrayPrefList != null) {
+
             for (String value : getStringArrayPrefList) {
                 set.add(value);
             }
             array_list.clear();
             array_list.addAll(set);
-            Collections.reverse(array_list);
-            arrayAdapter = new ArrayAdapter(SearchActivity.this, android.R.layout.simple_list_item_multiple_choice, array_list);
+            arrayAdapter = new ArrayAdapter(SearchActivity.this,R.layout.searchlog_card,R.id.log,array_list);
             listView.setAdapter(arrayAdapter);
 
         }
     }
 
+
     // 매개변수로 shared에 저장할 key값과 ArrayList<String> 타입의 List를 넣어주면 shared에 저장하는 메소드
     private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+        ///Json array
+        JSONArray a = new JSONArray();
 
-        for (int i = getStringArrayPrefList.size()-1; i >=0; i--) {
-            a.put(getStringArrayPrefList.get(i));
-        }
         for (int i = 0; i < values.size(); i++) {
             a.put(values.get(i));
         }
@@ -320,10 +307,12 @@ public class SearchActivity extends AppCompatActivity {
                     String url = a.optString(i);
                     urls.add(url);
                 }
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+
         return urls;
     }
 
--- app/src/main/java/com/example/vegeproject/search/SearchResult.java
+++ app/src/main/java/com/example/vegeproject/search/SearchResult.java
@@ -3,6 +3,7 @@ package com.example.vegeproject.search;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
